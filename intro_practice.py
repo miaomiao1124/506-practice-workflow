501c58a33f1f705db744a28e02d0cc8ec5b0568e
Miaoxuan Zhang <149809146+miaomiao1124@users.noreply.github.com>
2024-08-09 03:51:18 +0000

    intro_opractive

---
+++ intro_practice.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+name='Miaoxuan Zhang'
+print(type(name))
+age=40
+print(type(age))
+shopping_list=['milk','fruit','egg','vegetable','bread']
+print(type(shopping_list))
+patient_info={'name':'Miaoxuan','age':'40','gender':'female'}
+print(type(patient_info))
+
+def final_score(exam,assignment):
+    if type(exam)!=int or type(assignment)!=int:
+        return'Please enter a valid integer'
+    else :
+        caculation=exam+assignment
+    output=caculation
+    if caculation>=80:
+        print('congratulations, you did it')
+    else:
+        print('I am sorry, you have to retake the course')
+final_score(50,30)
+final_score(20,30)
